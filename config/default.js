39eb8449327950f379427770db81588cc28190c4
Maxim Piven <shpax1@gmail.com>
2019-04-23 09:20:56 +0300

    removed extra config

--- config/default.js
+++ config/default.js
@@ -2,10 +2,6 @@ const path = require('path');
 const awilix = require('awilix');
 
 module.exports = {
-  getUpdates: {
-    enabled: false,
-    interval: 1000,
-  },
   mysql: {
     host: '0.0.0.0',
     dialect: 'mysql',
@@ -14,10 +10,5 @@ module.exports = {
     database: 'tg_new'
   },
 
-  logger: {
-    path: 'log'
-  },
-
   botId: '<bot_id>',
-  sendInterval: 800,
 };
