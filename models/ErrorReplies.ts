40512ec9eb9eba8124d4b520130e5a2e349717b0
Maxim Piven <shpax1@gmail.com>
2019-04-23 10:59:14 +0300

    reply text

--- models/ErrorReplies.ts
+++ models/ErrorReplies.ts
@@ -6,12 +6,12 @@ let ERROR_REPLIES:{[index:number]: string} = {
   [CODE.QUEST_HAS_NO_QUESTIONS]: 'в викторине нет вопросов',
   [CODE.QUEST_IN_PROGRESS]: 'кажется ты уже и так на пол пути к успеху',
   [CODE.QUEST_NOT_FOUND]: 'ой, я ничего не нашел',
-  [CODE.QUEST_VISITED]: 'кажется, ты уже тут отвечал',
+  [CODE.QUEST_VISITED]: 'кажется, ты уже тут отвечал. Второй раз не получится. А пока можно посмотреть серию Игры Престолов, да и премьера Мстителей не за горами',
   [CODE.OPTION_NOT_VALID]: 'этого варианта тут нет',
   [CODE.EMAIL_MISSING]: 'это не почта!',
   [CODE.TEXT_MISSING]: 'это не ответ!',
   [CODE.CONTACT_MISSING]: 'ты не отправил контакт',
-  [CODE.NOTHING_IN_PROGRESS]: 'хм, кажется мне нечего тебе сказать. Попробуй /start'
+  [CODE.NOTHING_IN_PROGRESS]: 'хм, мне нечего тебе сказать. Попробуй /start'
 }
 
 class ErrorReply {
