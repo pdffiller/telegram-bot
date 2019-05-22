f9ed1a90aacbb9f5d0bb104a2b2bef71cee7015e
Maxim Piven <shpax1@gmail.com>
2019-05-22 13:36:55 +0300

    added user validation

--- db/models/User.ts
+++ db/models/User.ts
@@ -1,15 +1,32 @@
-import { Table, Column, Model } from 'sequelize-typescript';
+import { Table, Column, Model, BeforeUpdate, BeforeCreate } from 'sequelize-typescript';
 
 @Table({ tableName: 'users' })
 class User extends Model<User> {
 
+
+  static getStringValue(str: string | null) {
+    if (!str) return null;
+
+    const match = str.match(/\w+/);
+    if (!match) return null;
+    return match[1];
+  }
+
+  @BeforeCreate
+  @BeforeUpdate
+  static validate(user: User) {
+    user.username = this.getStringValue(user.username);
+    user.last_name = this.getStringValue(user.last_name);
+    user.first_name = this.getStringValue(user.first_name);
+  }
+
   @Column telegramId: number;
 
-  @Column first_name: string;
+  @Column first_name: string | null;
   
-  @Column last_name: string;
+  @Column last_name: string | null;
 
-  @Column username: string;
+  @Column username: string | null;
 
   @Column company: string;
 
